fe54b1d8051c31d70bc3348968d380769d0b7f6b
Jessica Blackburn <jblackburn@soundexchange.com>
2014-09-18 16:13:56 -0400

    Add support Amazon and Fedora

--- manifests/deps/redhat.pp
+++ manifests/deps/redhat.pp
@@ -3,9 +3,8 @@
 # This module manages awscli dependencies for redhat $::osfamily.
 #
 class awscli::deps::redhat {
-  include ::epel
-  Package { require => Class['epel'] }
-
-  if ! defined(Package['python-devel']) { package { 'python-devel': ensure => installed } }
-  if ! defined(Package['python-pip']) { package { 'python-pip': ensure => installed } }
+  if ! $::operatingsystem in ['Amazon', 'Fedora'] {
+    include ::epel
+    Package { require => Class['epel'] }
+  }
 }
